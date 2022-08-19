c60308bc22fc03134dbcd25730e5bf9e2f6e13ad
goodwasif <muhammad.wasif@ideofuzion.com>
2022-08-19 10:14:24 +0500

    chore: initial commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
 import "./styles.css";
 import logo from "../../assets/react.svg";
+import { useRef } from "react";
 
 const Home = () => {
+  const inputRef = useRef(null);
+
+  const getRefValue = () => {
+    console.log(inputRef?.current?.value);
+  };
+
   return (
     <div id="home">
       <div id="home-content">
         <div className="page-title">
           <img src={logo} alt="" />
           <h2>React is awesome!</h2>
+          <div>
+            <input type="text" ref={inputRef} />
+            <button onClick={getRefValue}>Get Value</button>
+          </div>
         </div>
         <br />
         <p>Welcome!</p>
